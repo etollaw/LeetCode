215e8eaf813c2a03edc6d6ae37df67f197d70bf8
Eldad Tolla <166652628+etollaw@users.noreply.github.com>
2025-02-20 09:56:16 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (48.03%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -1,10 +1,3 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        set1 = set(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
-        set2 = set()
-
-        for char in sentence:
-            if char not in set2:
-                set2.add(char)
-
-        return set1 == set2
+        return True if len(set(sentence)) == 26 else False
